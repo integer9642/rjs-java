efb28f8099e30b81df3201f4966d01ef754e0096
jungsoo <wjdtn402@naver.com>
2021-12-20 19:30:34 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        //국룰
+        System.out.println("hello java!!!");
+
+        //변수
+        int num = 5;
+        System.out.println(num);
+
+        String rjs = "Hello RJS";
+        System.out.println(rjs);
+
+        //final로 선언해주면 더이산 변수 재정의 불가
+        final int finalNumber = 1;
+        System.out.println(finalNumber);
+
+    }
+}
