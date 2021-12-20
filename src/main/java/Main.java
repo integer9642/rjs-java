b5c0323fd2de20e07dc725f8097df69b53a00996
jungsoo <wjdtn402@naver.com>
2021-12-20 19:46:03 +0900

    자바 공부 첫날

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //국룰
         System.out.println("hello java!!!");
 
@@ -14,5 +14,39 @@ public class Main {
         final int finalNumber = 1;
         System.out.println(finalNumber);
 
+        short s = 1;
+        System.out.println(s);
+
+        int a = 3;
+        System.out.println(a);
+
+        long b = 4;
+        System.out.println(b);
+
+        //float 뒤에는 F
+        float f = 5.5F;
+        System.out.println(f);
+
+        double d = 5.5;
+        System.out.println(d);
+
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MIN_VALUE);
+
+        char c = 'A';
+        System.out.println(c);
+
+        boolean fact = true;
+        System.out.println(fact);
+        fact = false;
+        System.out.println(fact);
+
+        byte data = 'd';
+        System.out.println(data);
     }
 }
